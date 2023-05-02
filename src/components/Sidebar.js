2b9f66137de3b2ee321e1a555748dfd777d40414
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-05-02 07:23:48 +0300

    fixing some warnings

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useContext, useEffect } from "react";
 import { Col, ListGroup, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -25,7 +26,7 @@ function Sidebar() {
     }
 
     socket.off("notifications").on("notifications", (room) => {
-        if (currentRoom != room) dispatch(addNotifications(room));
+        if (currentRoom !== room) dispatch(addNotifications(room));
     });
 
     useEffect(() => {
@@ -35,6 +36,7 @@ function Sidebar() {
             socket.emit("join-room", "general");
             socket.emit("new-user");
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     socket.off("new-user").on("new-user", (payload) => {
@@ -69,18 +71,18 @@ function Sidebar() {
             <h2 style={{marginTop:'20px'}}>My Groups</h2>
             <ListGroup>
                 {rooms.map((room, idx) => (
-                    <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" , margin:'10px'}}>
+                    <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room === currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" , margin:'10px'}}>
                         {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
                     </ListGroup.Item>
                 ))}
             </ListGroup>
             <h2 style={{marginTop:'20px'}}>Members</h2>
             {members.map((member) => (
-                <ListGroup.Item key={member.id} style={{ cursor: "pointer" ,  margin:'10px'}} active={privateMemberMsg?._id == member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
+                <ListGroup.Item key={member.id} style={{ cursor: "pointer" ,  margin:'10px'}} active={privateMemberMsg?._id === member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
                     <Row>
                         <Col xs={2} className="member-status">
                             <img src={member.picture} className="member-status-img" />
-                            {member.status == "online" ? <i className="fas fa-circle sidebar-online-status"></i> : <i className="fas fa-circle sidebar-offline-status"></i>}
+                            {member.status === "online" ? <i className="fas fa-circle sidebar-online-status"></i> : <i className="fas fa-circle sidebar-offline-status"></i>}
                         </Col>
                         <Col xs={9}>
                             {member.name}
